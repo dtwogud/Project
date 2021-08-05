c466af74c1e02bc004f55f90ce285649bb0d07f1
dtwogud <dtwogud@nate.com>
2021-08-06 02:14:14 +0900

    style수정

--- movie_app/movie_app_2021/src/App.js
+++ movie_app/movie_app_2021/src/App.js
@@ -1,52 +1,20 @@
 import React from 'react';
-// import PropTypes from "prop-types";
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
+import {HashRouter, Route} from 'react-router-dom';
+import About from "./routes/About"
+import Home from "./routes/Home"
+import Details from "./routes/Details"
+import Navigation from "./components/Navigation"
+import "./App.css"
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies_1 : []
-  }
-
-// render()하면 호출되는 life cycle method
-  getMovies = async () => {
-    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
-    // console.log(movies.data.data.movies)
-    console.log(movies)
-    // this.setState({movies_1:movies})
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount() {
-    this.getMovies()
-  }
-
-  render() {
-    const {isLoading, movies} = this.state
-    return (
-    <sectios class="container">
-      {isLoading ? (
-        <div class="loader">
-        <span class="loader__text">Loading...</span>
-        </div>
-      ) : (
-        <div class="movies">
-          {movies.map(movie => (
-        <Movie
-          key={movie.id}
-          id={movie.id}
-          year={movie.year}
-          title={movie.title}
-          summary={movie.summary}
-          poster={movie.medium_cover_image}
-          genres={movie.genres}
-        />
-      ))}
-    </div>
-    )}
-    </sectios>)
-  }
+function App() {
+  return(
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:title" component={Details} />
+    </HashRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ movie_app/movie_app_2021/src/routes/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from '../components/Movie';
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies_1 : []
+  }
+
+// render()하면 호출되는 life cycle method
+  getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    // console.log(movies.data.data.movies)
+    console.log(movies)
+    // this.setState({movies_1:movies})
+    this.setState({movies, isLoading: false})
+  }
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  render() {
+    const {isLoading, movies} = this.state
+    return (
+    <sectios class="container">
+      {isLoading ? (
+        <div class="loader">
+        <span class="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+        />
+      ))}
+    </div>
+    )}
+    </sectios>)
+  }
+}
+
+export default Home;
