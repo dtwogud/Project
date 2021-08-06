4a85d57f23aaf793da73ce5045fcf4a1294a13b0
dtwogud <dtwogud@nate.com>
2021-08-06 15:28:53 +0900

    frame 수정

--- movie_app/movie_app_2021/src/components/Movie.js
+++ movie_app/movie_app_2021/src/components/Movie.js
@@ -6,32 +6,32 @@ import "./Movie.css";
 // component가 state가 필요 없을경우 class일필요X -> function사용
 function Movie({id,year,title,summary,poster,genres}){
   return (
-    <Link to={{
-      pathname: `/movie/${title}`,
-      state:{
-        year,
-        title,
-        summary,
-        poster,
-        genres
-      }
-    }} >
-      <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
-            {genres.map((genre, index) => (
+    <div className="movie">
+      <Link to={{
+        pathname: `/movie/${title}`,
+        state:{
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }} >
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
             </li>
-            ))}
-            </ul>
-          <p className="movie__summary">{summary.slice(0,140)}...</p>
-        </div>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0,140)}...</p>
+      </div>
+      </Link>
     </div>
-  </Link>
   )
 }
 
